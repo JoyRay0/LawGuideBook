2054b5912374dfe59cdeee91ac0d126b7073121d
Joy Ray <joys57725@gmail.com>
2026-02-22 20:44:35 +0600

    Added links

--- app/src/main/java/com/rk_softwares/lawguidebook/Helper/ApiLinks.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Helper/ApiLinks.kt
@@ -2,7 +2,7 @@ package com.rk_softwares.lawguidebook.Helper
 
 object ApiLinks {
 
-    private val main_link =""
+    private val main_link ="https://lawguidebook.rksoftwares.fun"
 
     fun getChatLink() : String { return main_link+"" }
 
