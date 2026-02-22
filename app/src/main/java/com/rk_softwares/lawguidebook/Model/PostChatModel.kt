b11ef8d5f0089aade3bd458f6c54b393e92d9712
Joy Ray <joys57725@gmail.com>
2026-02-22 20:32:42 +0600

    Modified model class

--- app/src/main/java/com/rk_softwares/lawguidebook/Model/PostChatModel.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Model/PostChatModel.kt
@@ -1,12 +1,21 @@
 package com.rk_softwares.lawguidebook.Model
 
-data class ChatModel(
+data class PostChatModel(
 
     val id : Long = 0L,
     val user_message : String = "",
     val ai_message : String = "",
     val isUser : Boolean = false,
-    val deviceId : String = ""
+    val deviceId : String = "",
+
+)
+
+data class ChatModel(
+
+    val id : Int = 0,
+    val message : String = "",
+    val sender : String = "",
+    val timestamp : String = ""
 
 )
 
