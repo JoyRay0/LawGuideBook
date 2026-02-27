c76329d95a11821a3622ce29ba303b5479117f78
Joy Ray <joys57725@gmail.com>
2026-02-27 11:06:10 +0600

    Fixed live internet status

--- app/src/main/java/com/rk_softwares/lawguidebook/Helper/InternetChecker.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Helper/InternetChecker.kt
@@ -3,31 +3,31 @@ package com.rk_softwares.lawguidebook.Helper
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
-import android.widget.Toast
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
 
-object InternetChecker {
+interface InternetStatus{
+    fun isInternet(internet : Boolean)
+}
+
+class InternetChecker(
+    private val view : InternetStatus
+) {
 
     @Composable
-    fun liveInternetStatus(context: Context) : Boolean {
+    fun LiveInternetStatus(context: Context){
 
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        var isConnected by remember { mutableStateOf(false) }
 
         DisposableEffect(Unit) {
 
             val callback = object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
-                    isConnected = true
+                    view.isInternet(true)
                 }
 
                 override fun onLost(network: Network) {
-                    isConnected = false
+                    view.isInternet(false)
                 }
             }
 
@@ -37,10 +37,6 @@ object InternetChecker {
                 cm.unregisterNetworkCallback(callback)
             }
         }
-
-        return isConnected
-
-        //Toast.makeText(context,if (isConnected) "On" else "Off", Toast.LENGTH_SHORT).show()
 
     }
 
