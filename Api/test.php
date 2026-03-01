547e1a42407bcb7ae930f7e86c36e8c8d88444af
Joy Ray <joys57725@gmail.com>
2026-03-01 14:41:19 +0600

    Modified api

--- Api/test.php
+++ Api/test.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-$data = json_decode(file_get_contents("php://input", ),true);
+$data = json_decode(file_get_contents("php://input"),true);
 
 if($data['user_message'] == "hi"){
 
