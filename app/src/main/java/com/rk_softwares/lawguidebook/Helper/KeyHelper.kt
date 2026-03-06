f7ced2369fe0781d0a36717e4896d096a0089658
Joy Ray <joys57725@gmail.com>
2026-03-06 13:57:21 +0600

    New Key added

--- app/src/main/java/com/rk_softwares/lawguidebook/Helper/KeyHelper.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Helper/KeyHelper.kt
@@ -3,10 +3,9 @@ package com.rk_softwares.lawguidebook.Helper
 object KeyHelper {
 
     fun sendQuestion_IntentKey() : String { return "question" }
-
     fun sendTitle_IntentKey() : String { return "title" }
-
     fun otherApp_privacy_IntentKey() : String { return "other_app&privacy" }
+    fun calculationTitle_IntentKey() : String { return "calculation" }
 
 
 }
