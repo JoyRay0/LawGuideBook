944ec34bcb94550e565b3ba9b1d7516d7934308a
Joy Ray <joys57725@gmail.com>
2026-03-07 18:17:45 +0600

    Fixed live internt issuse

---
+++ app/src/main/java/com/rk_softwares/lawguidebook/View/Act_browser.kt
@@ -0,0 +1,259 @@
+package com.rk_softwares.lawguidebook.View
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Browser
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.rk_softwares.lawguidebook.Helper.ComposeHelper
+import com.rk_softwares.lawguidebook.Helper.InternetChecker
+import com.rk_softwares.lawguidebook.Helper.InternetStatus
+import com.rk_softwares.lawguidebook.Helper.KeyHelper
+import com.rk_softwares.lawguidebook.Helper.ShortMessageHelper
+import com.rk_softwares.lawguidebook.Helper.ThemeHelper
+import com.rk_softwares.lawguidebook.R
+import com.rk_softwares.lawguidebook.View.theme_main.LawGuideBookTheme
+import com.rk_softwares.lawguidebook.View.theme_main.LightNav
+import com.rk_softwares.lawguidebook.View.theme_main.LightStatusBar
+import com.rk_softwares.lawguidebook.View.theme_main.LightToolBar
+
+class Act_browser : ComponentActivity(), InternetStatus {
+
+    private lateinit var internetChecker: InternetChecker
+
+    //init-------------
+    private var isInternet = mutableStateOf(false)
+    private var website = mutableStateOf("")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            init()
+
+            ThemeHelper.SystemUi(
+                statusBarColor = LightStatusBar,
+                navColor = LightNav,
+                darkIcons = false
+            )
+
+            website.value = intent.getStringExtra(KeyHelper.lawWebsiteLink_IntentKey()) ?: ""
+
+            LawGuideBookTheme {
+
+                BrowserFullScreen(
+                    backClick = {
+                        finish()
+                        website.value = ""
+                    },
+                    website = website.value,
+                    internet = isInternet.value,
+                )
+
+            }
+
+            BackHandler {
+
+                finish()
+                website.value = ""
+
+            }
+        }
+    }//on create=================================
+
+    private fun init(){
+
+        internetChecker = InternetChecker(this, this)
+
+        internetChecker.onStart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        internetChecker.onStop()
+    }
+
+    override fun isInternet(internet: Boolean) {
+        isInternet.value = internet
+    }
+
+}//class==========================================
+
+
+
+@Preview(showBackground = true)
+@Composable
+private fun BrowserFullScreen(
+    backClick: () -> Unit = {},
+    website : String = "",
+    internet: Boolean = false
+) {
+
+    var isInternetDialogVisible by remember { mutableStateOf(false) }
+
+    if (internet) isInternetDialogVisible = false else isInternetDialogVisible = true
+
+
+    Scaffold(
+        topBar = { Toolbar(
+            backClick = { backClick() }
+        ) },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+
+        Box(
+
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+
+        ) {
+            if (internet){
+
+                WebView(
+                    websiteLink = website
+                )
+
+            }
+
+            if (isInternetDialogVisible){
+
+                ComposeHelper.InternetDialog(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter),
+                    closeClick = { isInternetDialogVisible = false },
+                    openClick = { isInternetDialogVisible = false }
+                )
+
+            }
+
+        }//box
+
+    }//scaffold
+
+}//fun end
+
+@Preview(showBackground = true)
+@Composable
+private fun Toolbar(
+    backClick : () -> Unit = {},
+) {
+
+    Box(
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = LightToolBar)
+
+    ) {
+
+        Row(
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(3.dp)
+
+        ) {
+
+            IconButton(
+                onClick = { backClick() },
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .align(Alignment.CenterVertically)
+            ) {
+
+                Icon( painter = painterResource(R.drawable.ic_back),
+                    contentDescription = "Back",
+                    tint = Color(0xFFFFFFFF),
+                    modifier = Modifier
+                        .wrapContentWidth()
+
+                )
+
+            }
+
+        }//row
+
+    }//box
+
+}//fun end
+
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+private fun WebView(
+    modifier: Modifier = Modifier,
+    websiteLink : String = ""
+) {
+
+    Box(
+
+        modifier = modifier
+            .fillMaxSize()
+
+    ) {
+
+
+        AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = WebViewClient()
+                settings.apply {
+
+                    javaScriptEnabled = true
+                    domStorageEnabled = true
+                    loadWithOverviewMode = true
+                    useWideViewPort = true
+                    builtInZoomControls = true
+                    displayZoomControls = false
+
+                    allowFileAccess = false
+                    allowContentAccess = false
+
+                    allowFileAccessFromFileURLs = false
+                    allowFileAccessFromFileURLs = false
+
+                }
+                loadUrl(websiteLink)
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+
+    }//box
+
+}//fun end
