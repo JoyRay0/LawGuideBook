9b826872964c587a3035ff120a54313534036425
Joy Ray <joys57725@gmail.com>
2026-03-28 12:14:46 +0600

    New intent key added

--- app/src/main/java/com/rk_softwares/lawguidebook/Helper/KeyHelper.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Helper/KeyHelper.kt
@@ -4,6 +4,7 @@ object KeyHelper {
 
     fun sendQuestion_IntentKey() : String { return "question" }
     fun sendTitle_IntentKey() : String { return "title" }
+    fun sendTableName_IntentKey() : String { return "table_name" }
     fun otherApp_privacy_IntentKey() : String { return "other_app&privacy" }
     fun calculationTitle_IntentKey() : String { return "calculation" }
     fun lawWebsiteLink_IntentKey() : String { return "law_website" }
