cba98c64059e1a776f2021ac2b213db79924c5fb
Joy Ray <joys57725@gmail.com>
2026-07-04 18:11:32 +0600

    Fix: public CO to private CO, user_input default value null to 0  | Add: insert , updateQuiz, getAll functions

--- app/src/main/java/com/rk_softwares/lawguidebook/Database/QuizDatabase.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Database/QuizDatabase.kt
@@ -1,13 +1,15 @@
 package com.rk_softwares.lawguidebook.Database
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.rk_softwares.lawguidebook.Model.QuizData
 
 class QuizDatabase(val context: Context) : SQLiteOpenHelper(context, "quiz.db", null, 1) {
 
-    companion object{
+    private companion object{
 
         const val TABLE_NAME = "quiz"
         const val ID = "id"
@@ -33,7 +35,7 @@ $OPTIONS_B TEXT NOT NULL,
 $OPTIONS_C TEXT NOT NULL, 
 $OPTIONS_D TEXT NOT NULL, 
 $ANSWER TEXT NOT NULL, 
-$USER_INPUT TEXT DEFAULT NULL)""".trimIndent()
+$USER_INPUT TEXT DEFAULT 0)""".trimIndent()
 
         db?.execSQL(sql)
 
@@ -43,9 +45,93 @@ $USER_INPUT TEXT DEFAULT NULL)""".trimIndent()
         TODO("Not yet implemented")
     }
 
-    fun insert(){}//fun end
+    fun insert(
+        title: String,
+        optionA : String,
+        optionB : String,
+        optionC : String,
+        optionD : String,
+        answer : String,
+    ){
 
-    fun getAll(){}//fun end
+        if (title.isEmpty() || optionA.isEmpty() || optionB.isEmpty() || optionC.isEmpty() || optionD.isEmpty()
+            || answer.isEmpty()){
+
+            return
+
+        }
+
+        if (checkDuplicate(title)) return
+
+        val db = dbOpen(true)
+
+        val cv = ContentValues()
+
+        try {
+
+            cv.put(TITLE, title)
+            cv.put(OPTIONS_A, optionA)
+            cv.put(OPTIONS_B, optionB)
+            cv.put(OPTIONS_C, optionC)
+            cv.put(OPTIONS_D, optionD)
+            cv.put(ANSWER, answer)
+
+            db.insert(TABLE_NAME, null, cv)
+
+        }catch (e : Exception){
+
+            e.printStackTrace()
+
+        }
+
+
+    }//fun end
+
+    fun getAll(): List<QuizData>{
+
+        val db = dbOpen()
+
+        val quizList : MutableList<QuizData> = mutableListOf()
+
+        var cursor : Cursor? = null
+
+        try {
+
+            cursor = db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+
+            while (cursor.moveToNext()){
+
+                val title = cursor.getString(cursor.getColumnIndexOrThrow(TITLE))
+                val optionA = cursor.getString(cursor.getColumnIndexOrThrow(OPTIONS_A))
+                val optionB = cursor.getString(cursor.getColumnIndexOrThrow(OPTIONS_B))
+                val optionC = cursor.getString(cursor.getColumnIndexOrThrow(OPTIONS_C))
+                val optionD = cursor.getString(cursor.getColumnIndexOrThrow(OPTIONS_D))
+                val answer = cursor.getString(cursor.getColumnIndexOrThrow(ANSWER))
+                val userInput = cursor.getInt(cursor.getColumnIndexOrThrow(USER_INPUT))
+
+                quizList.add(QuizData(
+                    title = title,
+                    optionA = optionA,
+                    optionB = optionB,
+                    optionC = optionC,
+                    optionD = optionD,
+                    answer = answer,
+                    userInput = userInput
+                ))
+
+            }
+
+        }catch (e : Exception){
+
+            e.printStackTrace()
+
+        }finally {
+            cursor?.close()
+        }
+
+        return quizList
+
+    }//fun end
 
     fun deleteAll(){
 
@@ -63,7 +149,33 @@ $USER_INPUT TEXT DEFAULT NULL)""".trimIndent()
 
     }//fun end
 
-    fun updateQuiz(){}//fun end
+    fun updateQuiz(
+        title: String,
+        userSelectedItem : Int
+    ){
+
+        if (title.isEmpty() || userSelectedItem > 0) return
+
+        if (!checkDuplicate(title)) return
+
+        val db = dbOpen(true)
+
+        val cv = ContentValues()
+
+        try {
+
+            cv.put(TITLE, title)
+            cv.put(USER_INPUT, userSelectedItem)
+
+            db.update(TABLE_NAME, cv, "$TITLE = ?", arrayOf(title))
+
+        }catch (e : Exception){
+
+            e.printStackTrace()
+
+        }
+
+    }//fun end
 
     private fun checkDuplicate(title : String) : Boolean{
 
