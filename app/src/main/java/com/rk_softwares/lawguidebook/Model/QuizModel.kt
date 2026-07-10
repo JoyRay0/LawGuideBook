efd80dff0ef2eac51c79a34846570dd45aca7ee0
Joy Ray <joys57725@gmail.com>
2026-07-10 14:18:46 +0600

    Fix: quiz from server bug

--- app/src/main/java/com/rk_softwares/lawguidebook/Model/QuizModel.kt
+++ app/src/main/java/com/rk_softwares/lawguidebook/Model/QuizModel.kt
@@ -1,13 +1,20 @@
 package com.rk_softwares.lawguidebook.Model
 
+import android.util.Log
 import com.google.gson.annotations.SerializedName
 import com.rk_softwares.lawguidebook.Database.QuizDatabase
+import com.rk_softwares.lawguidebook.Helper.ApiLinks
+import com.rk_softwares.lawguidebook.Helper.OkHttpWrapper
+import com.rk_softwares.lawguidebook.Helper.header
 import kotlinx.serialization.Serializable
 
 data class Quiz(
 
     val status : String = "",
     val message : String = "",
+
+    @SerializedName("version")
+    val quizVersion : String = "",
 
     @SerializedName("data")
     val data : List<QuizData> = emptyList()
@@ -31,6 +38,99 @@ class QuizModel(
     private val db : QuizDatabase
 ) {
 
+    fun quizFromServer(
+        onSuccess : (Quiz) -> Unit = {},
+        onFailed : (Boolean) -> Unit = {}
+    ){
+
+        OkHttpWrapper()
+            .url(ApiLinks.getQuiz())
+            .header()
+            .execute(Quiz::class.java, onSuccess = { result ->
+
+                if (result.status == "Success"){
+
+                    onSuccess(result)
+
+                }else{
+
+                    onFailed(true)
+
+                }
+
+            }, onFailed = {onFailed(it)}, onError = {})
+
+    }//fun end
+
+    fun insertQuiz(
+        title: String,
+        optionA: String,
+        optionB: String,
+        optionC: String,
+        optionD: String,
+        answer: String,
+        version: String
+    ){
+
+        if (title.isEmpty() || optionA.isEmpty() || optionB.isEmpty() || optionC.isEmpty() || optionD.isEmpty()
+            || answer.isEmpty() || version.isEmpty()){
+
+            return
+
+        }
+
+        db.insert(
+            title = title,
+            optionA = optionA,
+            optionB = optionB,
+            optionC = optionC,
+            optionD = optionD,
+            answer = answer,
+            version = version
+        )
+
+    }
+
+    fun getAllQuizData(): List<QuizData>{
+
+        val data = db.getAll()
+
+        return data
+
+    }
+
+    fun updateQuzData(
+        title: String,
+        userInput: Int
+    ){
+
+        if (title.isEmpty() || userInput < 0) return
+
+        db.updateQuiz(title = title, userSelectedItem = userInput)
+
+    }
+
+    fun isNewVersion(version: String): Boolean{
+
+        if (version.isEmpty()) return false
+
+        val isNew = db.isNewVersion(version)
+
+        return isNew
+
+    }
+
+    fun deleteAllQuiz(){
+
+        db.deleteAll()
+
+    }
+
+    fun quizCount(tQuiz : (Int) -> Unit, cQuiz : (Int) -> Unit){        /* tQuiz = Total Quiz, cQuiz = Completed quiz */
+
+        db.quizCount( totalQuiz = { tQuiz(it) }, totalQuizCompleted = {cQuiz(it)} )
+
+    }
 
 
 }
